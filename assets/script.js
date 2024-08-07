cbc01eb7990f66139e1e691e0fa64f182cc43a03
Augusto Marques de Souza <augustomarquesdesouza@gmail.com>
2024-08-07 17:12:53 -0300

    Finalização site provisória

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,3 @@
 let changeIcon = function(icon) {
-    icon.classList.toggle('bi-x');
+  icon.classList.toggle('bi-x');
 }
-
-// const navbarFixed = document.querySelectorAll('.navbar');
-// let lastScrollTop = 0;
-// let sombraAtivada = false;
-
-// function adicionarSombra() {
-//   addEventListener('scroll', function() {
-//     const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-//     if (scrollTop > lastScrollTop && !sombraAtivada) { 
-//       navbarFixed.forEach(navbar => navbar.style.boxShadow = '0px 2px 5px rgba(0, 0, 0, 0.2)'); 
-//       sombraAtivada = true; 
-//     } else if (scrollTop === 0) { 
-//       navbarFixed.forEach(navbar => navbar.style.boxShadow = 'none'); 
-//       sombraAtivada = false; 
-//     }
-
-//     lastScrollTop = scrollTop;
-//   });
-// }
-
-// adicionarSombra(); 
