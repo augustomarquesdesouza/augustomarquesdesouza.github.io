56fcb31ed10b00b2469747d6b19c912ed4a24470
Augusto Marques de Souza <augustomarquesdesouza@gmail.com>
2024-09-10 16:25:16 -0300

    Adicionando efeito máquina de escrever

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,29 @@
-let changeIcon = function(icon) {
-  icon.classList.toggle('bi-x');
-}
+let changeIcon = function (icon) {
+  icon.classList.toggle("bi-x");
+};
 
 //Animação ao scroll
 function initAnimaScroll() {
-    const sections = document.querySelectorAll('.js-scroll');
-    const windowsMetade = window.innerHeight * 0.75;
-    
-    function animaScroll() {
-        sections.forEach((section) => {
-            const sectionTop = section.getBoundingClientRect().top;
-            const isSectionVisible = (sectionTop - windowsMetade) < 0;
-            if(isSectionVisible) {
-                section.classList.add('ativo');
-            }
-        })
-    };
-    animaScroll();
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowsMetade = window.innerHeight * 0.75;
 
-    window.addEventListener('scroll', animaScroll);
-};
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowsMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
 
 initAnimaScroll();
+//Animação Máquina de escrever
+new TypeIt("#element", {
+    speed: 80,
+    startDelay: 0
+  }).go();
